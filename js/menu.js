e9932e0adc7a521f2d9d316474eb87ae90cae7de
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-03-14 15:03:44 +0200

    adds styles

---
+++ js/menu.js
@@ -0,0 +1,65 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+
+  const body = document.querySelector('body');
+
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+    window.addEventListener('keydown', handlerEscModalClose);
+  });
+
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+    window.removeEventListener('keydown', handlerEscModalClose);
+  });
+
+})();
+
+// Реализация закрытия модалки при клике в бекдроп
+const menuBtnClose = document.querySelector('[close-menu]');
+const backdrop = document.querySelector('.backdrop');
+const body = document.querySelector('body');
+backdrop.addEventListener('click', onBackdropClick);
+function onBackdropClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    onModalClose();
+  }
+}
+
+// Функция закрытия модального окна при нажатии на Esc
+function handlerEscModalClose(evt) {
+  if (evt.code === 'Escape') {
+    onModalClose();
+  }
+}
+// Функция закрытия модального окна и снятия всех классов
+function onModalClose() {
+  backdrop.classList.add('is-hidden');
+  body.classList.remove('modal-open');
+  menuBtnClose.classList.remove('is-open');
+
+  window.removeEventListener('keydown', handlerEscModalClose);
+}
+
+// закрытие модального окна при клике по элементам навигации
+const mainModalMenuListItems = document.querySelector('.mobile-menu__link-list');
+mainModalMenuListItems.addEventListener('click', onMainMenuItemsClick);
+
+
+function onMainMenuItemsClick(evt) {
+  if (!evt.target.classList.value === 'mobile-menu__list-item') {
+    return;
+  }
+  onModalClose();
+}
+
+const onlineBtn = document.querySelector('.contact-us__online');
+onlineBtn.addEventListener('click', onOnlineBtnClick);
+function onOnlineBtnClick(evt) {
+  if (!evt.target.classList.value === 'contact-us__online') {
+    return;
+  }
+  onModalClose();
+}
